8d8d24843c1b666ae66f28bd786a8cc8cbc13e57
rahulsahay19 <rahulsahay19@hotmail.com>
2021-10-23 00:34:14 +0530

    added spring cloud server

--- limits-service/src/main/java/com/myview/limits/service/controller/LimitsController.java
+++ limits-service/src/main/java/com/myview/limits/service/controller/LimitsController.java
@@ -1,14 +1,18 @@
 package com.myview.limits.service.controller;
 
 import com.myview.limits.service.bean.Limits;
+import com.myview.limits.service.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
-
+    @Autowired
+    private Configuration configuration;
     @GetMapping("/limits")
     public Limits getLimits(){
-        return new Limits(1,1000);
+        return new Limits(configuration.getMin(), configuration.getMax());
+        //return new Limits(1,1000);
     }
 }
