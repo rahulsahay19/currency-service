625df5070fca8a77080e54777f6d8640a6870503
rahulsahay19 <rahulsahay19@hotmail.com>
2021-10-27 14:04:08 +0530

    added currency conversion

---
+++ currency-conversion-service/src/main/java/com/myview/currency/conversion/service/CurrencyConversionApplication.java
@@ -0,0 +1,13 @@
+package com.myview.currency.conversion.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConversionApplication.class, args);
+    }
+
+}
---
+++ currency-conversion-service/src/test/java/com/myview/currency/conversion/service/CurrencyConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myview.currency.conversion.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
